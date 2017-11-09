9fe62f5fe0254a98a1a2af4294f9d7b77664e160
wrzosdev <wrzosdev@gmail.com>
2017-11-09 04:32:19 +0100

    Konfiguracja ViewPagera - Implementacja i zasilenie

--- app/src/main/java/pl/com/example/dietplus/MainActivity.kt
+++ app/src/main/java/pl/com/example/dietplus/MainActivity.kt
@@ -1,9 +1,13 @@
 package pl.com.example.dietplus
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v7.app.AppCompatActivity
+import com.github.florent37.materialviewpager.header.HeaderDesign
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
 import pl.com.example.dietplus.model.FoodIngridient
 
 class MainActivity : AppCompatActivity() {
@@ -21,9 +25,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initPager()
+    }
+
+    private fun initPager() {
+        materialpager.viewPager.adapter = object : FragmentStatePagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int) = Fragment()
+            override fun getCount() = ingridientArray.size
+            override fun getPageTitle(position: Int) = ingridientArray[position].name
+        }
+        materialpager.setMaterialViewPagerListener({ page ->
+            HeaderDesign.fromColorResAndDrawable(R.color.black, ingridientArray[page].drawable)
+        })
+        materialpager.viewPager.offscreenPageLimit = materialpager.viewPager.adapter.count
+
+        materialpager.pagerTitleStrip.setViewPager(materialpager.viewPager)
     }
 
     companion object {
-       private val FOOD_ING_TYPE = object : TypeToken<List<FoodIngridient>>(){}.type
+        private val FOOD_ING_TYPE = object : TypeToken<List<FoodIngridient>>() {}.type
     }
 }
